2187d35bcbde40b67dc4e37dbd29cf8437291195
F.Martin <ferranleal@gmail.com>
2016-06-01 17:10:48 +0200

    Add files via upload

--- Views/Hello/world.php
+++ Views/Hello/world.php
@@ -1,1 +1,15 @@
-<?php
+    <h1>Hello <?=$example?>!</h1>
+    <p class="lead">This is a "Hello World!" example, passing a $var in the URL like <code>hello/John</code>.</p>
+    
+    <section style="padding-top: 20px">
+        <h3>Edit this "Hello World!" example</h3>
+        <p>
+            The Controller is located on <code>Controller/Default.php</code>, edit this file and change it, go ahead!
+        </p>
+
+        <h3>Twitter</h3>
+        <p>
+            Follow <a href="http://www.twitter.com/pulseframework" target="_blank">pulseframework</a> on Twitter to receive the very latest news
+            and updates about the framework.
+        </p>
+    </section>
